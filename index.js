4c47f25a51f64780f621a00dbc07c5d3ab83f34c
Ziggy <davidarogers@protonmail.com>
2021-09-09 13:29:24 -0700

    geometry much saner

--- index.js
+++ index.js
@@ -10,39 +10,34 @@ window.onload = function () {
 		let SCREEN_HEIGHT = canvas.height = graph_canvas.height = Math.floor(0.75 * window.innerHeight);
 		if (SCREEN_WIDTH % 2) SCREEN_WIDTH--;
 		if (SCREEN_HEIGHT % 2) SCREEN_HEIGHT--;
+		console.log(`${SCREEN_WIDTH}, ${SCREEN_HEIGHT} screen dimensions`);
 
-		// Sizes for cartesian tiles given in powers of 2 exclusively.
-		const CELL_EXP = 3;
-		const CARTESIAN_TILE_LENGTH = Math.pow(2, CELL_EXP);
+		// Length of cartesian tiles given in powers of 2 exclusively.
+		const CELL_SCALE = 5; // Note the lower the scale, the most tiles.
+		const CARTESIAN_TILE_LENGTH = Math.pow(2, CELL_SCALE);
+		const CARTESIAN_TILE_COUNT = (SCREEN_WIDTH*SCREEN_HEIGHT) / Math.pow(CARTESIAN_TILE_LENGTH, 2);
+		console.log(`${CARTESIAN_TILE_COUNT} cartesian tiles rendered`);
 
 		// Isometric tile widths and heights, also note angle.
-		const TILE_WIDTH = 100;
-		const TILE_HEIGHT = 50;
-		const ANGLE = Math.atan(TILE_WIDTH / TILE_HEIGHT);
+		const ISO_TILE_WIDTH = 2 * CARTESIAN_TILE_LENGTH;
+		const ISO_TILE_HEIGHT = CARTESIAN_TILE_LENGTH;
+		// Calculating interior half angle for future refernce.
+		const ANGLE = Math.atan(ISO_TILE_WIDTH / ISO_TILE_HEIGHT);
 
-		// Now, for the maximum inscribable diamond we calculate the side lengths.
-		// This places a constraint of the maximum number of tiles that can be placed in
-		// the iso-space y direction.
-		const TOTAL_ISO_TILES = SCREEN_HEIGHT / (2 * Math.cos(ANGLE));
-
-		// We determine the total number of fittable tiles along iso-space y axis by first computing length of a tile side along the iso y-axis.
-		const ISO_Y_TILE_LENGTH = Math.sqrt(Math.pow(TILE_WIDTH / 2, 2) + Math.pow(TILE_HEIGHT / 2, 2));
-
-		// Total number of fittable tiles along iso y-axis obtained via:
-		const TOTAL_ISO_TILES_TILES = Math.floor(TOTAL_ISO_TILES / ISO_Y_TILE_LENGTH);
-
-		// This is will give the maximum number of fittable tiles in either direction.
-		const MAX_X = TOTAL_ISO_TILES_TILES;
-		const MAX_Y = TOTAL_ISO_TILES_TILES;
+		// Note that since the isometric edge tiles grows like the diagonal tiles, to obtain the total
+		// number of isometric tiles we need only square the number of vertical cartesian tiles from top
+		// to bottom.
+		const CARTESIAN_Y_COUNT = SCREEN_HEIGHT / CARTESIAN_TILE_LENGTH;
+		const ISO_TILE_COUNT = Math.pow(CARTESIAN_Y_COUNT, 2);
+		console.log(`${ISO_TILE_COUNT} total isometric tiles`);
 
 		// Center the context
-		context.translate((MAX_X * TILE_WIDTH / 1.5), 1.5);
+		context.translate((MAX_X * ISO_TILE_WIDTH / 1.5), 1.5);
 
 		drawGraphLines(CARTESIAN_TILE_LENGTH, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 		// This is the top view of the board as expressed in Cartesian coordinates.
 		const topDownGrid = createTileGrid(MAX_X, MAX_Y);
-		console.log(topDownGrid);
 		drawGrid(topDownGrid, "green");
 
 		function drawGraphLines (cell_length, s_width, s_height) {
@@ -72,9 +67,9 @@ window.onload = function () {
 				// Drawing boundary of isometric tiles.
 				context.beginPath();
 				context.moveTo(0,0);
-				context.lineTo(TILE_WIDTH / 2, TILE_HEIGHT / 2);
-				context.lineTo(0, TILE_HEIGHT);
-				context.lineTo(-TILE_WIDTH / 2, TILE_HEIGHT / 2);
+				context.lineTo(ISO_TILE_WIDTH / 2, ISO_TILE_HEIGHT / 2);
+				context.lineTo(0, ISO_TILE_HEIGHT);
+				context.lineTo(-ISO_TILE_WIDTH / 2, ISO_TILE_HEIGHT / 2);
 				context.closePath();
 				context.fillStyle = color;
 				context.fill();
@@ -83,8 +78,8 @@ window.onload = function () {
 		}
 
 		function mapToCartesian(x, y) {
-				const cartesianX = (x - y) * TILE_WIDTH / 2;
-				const cartesianY = (x + y) * TILE_HEIGHT / 2;
+				const cartesianX = (x - y) * ISO_TILE_WIDTH / 2;
+				const cartesianY = (x + y) * ISO_TILE_HEIGHT / 2;
 				return [cartesianX, cartesianY];
 		}
 
