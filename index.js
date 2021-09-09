da207ff9719c9e89ad1605f521c20040b69f1ea7
Ziggy <davidarogers@protonmail.com>
2021-09-09 07:39:32 -0700

    html and js start

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+		let canvas = document.getElementById("canvas");
+		let context = canvas.getContext("2d");
+		const width = canvas.width = window.innerWidth;
+		const height = canvas.width = window.innerWidth;
+}
