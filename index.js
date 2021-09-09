c2ca3d903a9a7a28f96a2dcce037d436e753a788
Ziggy <davidarogers@protonmail.com>
2021-09-09 08:17:27 -0700

    first tile

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ window.onload = function () {
 		let context = canvas.getContext("2d");
 		const width = canvas.width = 0.75*window.innerWidth;
 		const height = canvas.height = 0.75*window.innerHeight;
+		const TILE_WIDTH = 100;
+		const TILE_HEIGHT = 50;
 
-		context.fillStyle = 'green';
-		context.fillRect(0,0,width,height);
+		context.translate(width / 2, 50);
+		drawTile(0, 0, "green");
+
+		function drawTile(x, y, color) {
+				context.save();
+				context.translate(x,y);
+
+				context.beginPath();
+				context.moveTo(0,0);
+				context.lineTo(TILE_WIDTH / 2, TILE_HEIGHT / 2);
+				context.lineTo(0, TILE_HEIGHT);
+				context.lineTo(-TILE_WIDTH / 2, TILE_HEIGHT / 2);
+				context.closePath();
+				context.fillStyle = color;
+				context.fill();
+
+				context.restore();
+		}
 }
