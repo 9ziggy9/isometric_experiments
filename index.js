361a7366fb2e99d63459ca667f4af26555af33da
Ziggy <davidarogers@protonmail.com>
2021-09-09 07:56:19 -0700

    styling and basic html

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 window.onload = function () {
 		let canvas = document.getElementById("canvas");
 		let context = canvas.getContext("2d");
-		const width = canvas.width = window.innerWidth;
-		const height = canvas.width = window.innerWidth;
+		const width = canvas.width = 0.75*window.innerWidth;
+		const height = canvas.height = 0.75*window.innerHeight;
+
+		context.fillStyle = 'green';
+		context.fillRect(0,0,width,height);
 }
