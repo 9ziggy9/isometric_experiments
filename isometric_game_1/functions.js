e7e96819992b657712db85dc5ce78a0fbe04448c
Ziggy <davidarogers@protonmail.com>
2021-09-10 21:02:24 -0700

    preparing for server implementation

--- isometric_game_1/functions.js
+++ isometric_game_1/functions.js
@@ -1,103 +1,6 @@
-window.onload = function() {
-    // Get canvas and context
-    let canvas = document.getElementById("iso-canvas");
-    let context = canvas.getContext("2d");
-    let graph_canvas = document.getElementById("graph-canvas");
-    let graph_context = graph_canvas.getContext("2d");
-
-    // SETTING DESIRED SCALE OF TILES
-    let CELL_SCALE = 5; // Note the lower the scale, the most tiles.
-    const CARTESIAN_TILE_LENGTH = Math.pow(2, CELL_SCALE);
-
-    ///////////////////////////////////////////////////////////////////////////////////////
-    /*
-    	SCREEN HANDLING
-    */
-    ///////////////////////////////////////////////////////////////////////////////////////
-    // Screen dimensions MUST be divisible by cell size in order to facilitate clean geometry.
-    let SCREEN_WIDTH = canvas.width = graph_canvas.width = 30 * CARTESIAN_TILE_LENGTH;
-    let SCREEN_HEIGHT = canvas.height = graph_canvas.height = 15 * CARTESIAN_TILE_LENGTH;
-
-    console.log(`${SCREEN_WIDTH}, ${SCREEN_HEIGHT} screen dimensions`);
-    // END SCREEN HANDLING ///////////////////////////////////////////////////////////////
-
-    // Calculating total cartesian cell count.
-    const CARTESIAN_TILE_COUNT = (SCREEN_WIDTH * SCREEN_HEIGHT)
-					                       / Math.pow(CARTESIAN_TILE_LENGTH, 2);
-    console.log(`${CARTESIAN_TILE_COUNT} cartesian tiles rendered`);
-
-    // Isometric tile widths and heights, also note angle.
-    const ISO_TILE_WIDTH = 2 * CARTESIAN_TILE_LENGTH;
-    const ISO_TILE_HEIGHT = CARTESIAN_TILE_LENGTH;
-
-    const CARTESIAN_Y_COUNT = SCREEN_HEIGHT / CARTESIAN_TILE_LENGTH;
-    const ISO_TILE_COUNT = Math.pow(CARTESIAN_Y_COUNT, 2);
-    const ISO_MAX_X = Math.floor(0.75 * Math.sqrt(ISO_TILE_COUNT));
-    const ISO_MAX_Y = Math.floor(0.75 * Math.sqrt(ISO_TILE_COUNT));
-    console.log(`${ISO_TILE_COUNT} total isometric tiles`);
-
-    let TIME_STEP = 300;
-
-    // Center the isometric context relative to cartesian context and draw cartesian lines
-    context.translate(Math.floor(SCREEN_WIDTH / 2),
-											CARTESIAN_TILE_LENGTH * 4);
-
-    drawGraphLines(CARTESIAN_TILE_LENGTH,
-									 SCREEN_WIDTH,
-									 SCREEN_HEIGHT,
-									 graph_context);
-
-    ////////////////////////////////////////////////////////////////////////////////////////
-    /*
-    EVENT HANDLING
-    */
-    ////////////////////////////////////////////////////////////////////////////////////////
-    const spec_field = document.getElementById('set-button');
-    spec_field.addEventListener('click', (event) => {
-        event.preventDefault();
-        let freq_field = document.getElementById('freq-box');
-        TIME_STEP = freq_field.value;
-        console.log(TIME_STEP);
-    });
-
-    ///////////////////////////////////////
-    // !!!! RUN THE GOD DAMN THING !!!! //
-    /////////////////////////////////////
-    run(context);
-
-    ////////////////////////////////////////////////////////////////////////////////////////
-    /*
-			RUNTIME FUNCTIONS
-    */
-    ////////////////////////////////////////////////////////////////////////////////////////
-    function generateFrame(period) {
-        return new Promise(resolve => {
-            setTimeout(() => resolve('success: generate frame'), period);
-        });
-    }
-
-    function update() {
-        //Remember, when clearing we have previously translated context.
-        context.clearRect(-Math.floor(SCREEN_WIDTH / 2),
-            -CARTESIAN_TILE_LENGTH * 4,
-            canvas.width,
-            canvas.height);
-        const game_grid = createBoardGrid(ISO_MAX_X, ISO_MAX_Y);
-        drawBoard(game_grid, ISO_TILE_WIDTH, ISO_TILE_HEIGHT, "green", "block", context);
-    }
-
-    async function run() {
-        const result = await generateFrame(TIME_STEP);
-        update();
-				requestAnimationFrame(run);
-    }
-}
-
 //////////////////////////////////////////////////////////////////////////////////////
 /*
 	FUNCTION IMPLEMENTATIONS
-	NOTE: In order to truly decouple this file, you must be running a server in order to
-	import contents of functions.js.
 */
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -213,3 +116,12 @@ function drawBoard(grid,
             }
         }
 }
+
+module.exports = {
+		drawBoard,
+		createBoardGrid,
+		mapToCartesian,
+		drawBlock,
+		drawTile,
+		drawGraphLines,
+};
